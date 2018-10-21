bd244dc0b7ba8447900ceaafe54dbc1a56a0a8db
Andrew <andrew.1.kulikov@gmail.com>
2018-10-21 22:50:56 +0300

    Initial commit

---
+++ app/src/main/java/com/example/user_pc/myapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.user_pc.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val versionCode = BuildConfig.VERSION_CODE
+        val versionName = BuildConfig.VERSION_NAME
+        val helloTextView = findViewById<View>(R.id.text_view_id) as TextView
+        helloTextView.text = versionName + " "  + versionCode
+    }
+}
+
