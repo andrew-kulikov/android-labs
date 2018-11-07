012fff4e98f771d75ab9df9fa6af22f46b84325d
Andrew <andrew.1.kulikov@gmail.com>
2018-11-07 16:51:08 +0300

    Add release apk autosigning

--- app/src/main/java/com/example/kek/labs/MainActivity.java
+++ app/src/main/java/com/example/kek/labs/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.kek.labs;
 
+import android.util.Log;
 import android.widget.TextView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
         String versionName = BuildConfig.VERSION_NAME;
         TextView text1 = findViewById(R.id.text1);
         text1.setText(versionName + "   " + BuildConfig.VERSION_CODE);
+        Log.i("TAG", "On create");
     }
 }
