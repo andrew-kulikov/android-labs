80d00977af78b14981d59845f46e3d7bd74fa844
Andrew <andrew.1.kulikov@gmail.com>
2018-11-08 10:42:12 +0300

    Add permissions request

--- app/src/main/java/com/example/kek/labs/MainActivity.java
+++ app/src/main/java/com/example/kek/labs/MainActivity.java
@@ -1,20 +1,59 @@
 package com.example.kek.labs;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.TextView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private final int MY_REQUEST_CODE = 1488;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String versionName = BuildConfig.VERSION_NAME;
         TextView text1 = findViewById(R.id.text1);
-        text1.setText(versionName + "   " + BuildConfig.VERSION_CODE);
+        text1.setText(BuildConfig.VERSION_NAME + "   " + BuildConfig.VERSION_CODE);
         Log.i("TAG", "On create");
+
+        if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE)
+                == PackageManager.PERMISSION_GRANTED) {
+            TextView textImei = findViewById(R.id.imeiTextView);
+            textImei.setText(((TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE)).getDeviceId());
+        }
+
+        requestPermissions(new String[] {Manifest.permission.READ_PHONE_STATE}, MY_REQUEST_CODE);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_REQUEST_CODE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    try {
+                        TextView textImei = findViewById(R.id.imeiTextView);
+                        textImei.setText(((TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE)).getDeviceId());
+                    }
+                    catch (SecurityException e) {
+                        Log.e("ALARMA", "PERMISSION DENIED");
+                    }
+                } else {
+                    requestPermissions(new String[] {Manifest.permission.READ_PHONE_STATE}, MY_REQUEST_CODE);
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
     }
 }
