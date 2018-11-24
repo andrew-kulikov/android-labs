1e0daf36610aec149aee9f8f4305bd444857582f
Andrew <andrew.1.kulikov@gmail.com>
2018-11-24 15:30:57 +0300

    Migrate to androidX

--- app/src/main/java/com/example/kek/labs/Activity/MainActivity.java
+++ app/src/main/java/com/example/kek/labs/Activity/MainActivity.java
@@ -1,17 +1,19 @@
-package com.example.kek.labs;
+package com.example.kek.labs.Activity;
 
 import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
+import androidx.appcompat.app.AlertDialog;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.TextView;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.kek.labs.BuildConfig;
+import com.example.kek.labs.R;
 
 public class MainActivity extends AppCompatActivity {
     private final int MY_REQUEST_CODE = 1488;
