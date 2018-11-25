035977c2096d92a290f4553d394f48f85a55c908
Andrew <andrew.1.kulikov@gmail.com>
2018-11-25 23:23:58 +0300

    Add custom toolbar

--- app/src/main/java/com/example/kek/labs/Activity/MainActivity.java
+++ app/src/main/java/com/example/kek/labs/Activity/MainActivity.java
@@ -5,14 +5,21 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.kek.labs.R;
+import com.google.android.material.navigation.NavigationView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
     private NavController controller;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,18 @@ public class MainActivity extends AppCompatActivity {
 
         NavHostFragment host = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
         controller = host.getNavController();
+
+        NavigationView navView = findViewById(R.id.nav_view);
+        NavigationUI.setupWithNavController(navView, controller);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.about_black);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
     }
 
     @Override
@@ -35,6 +54,9 @@ public class MainActivity extends AppCompatActivity {
         switch (id) {
             case R.id.about_item:
                 controller.navigate(R.id.aboutFragment);
+                return true;
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
                 return true;
         }
         return super.onOptionsItemSelected(item);
