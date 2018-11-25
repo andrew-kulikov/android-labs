03d7a4ed5a01d7fd718cf2ee986ea5329c4bfd68
Andrew <andrew.1.kulikov@gmail.com>
2018-11-26 00:38:19 +0300

    Fix toolbar toggle

--- app/src/main/java/com/example/kek/labs/Activity/MainActivity.java
+++ app/src/main/java/com/example/kek/labs/Activity/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.kek.labs.Activity;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.example.kek.labs.R;
 import com.google.android.material.navigation.NavigationView;
 
-import androidx.appcompat.app.ActionBar;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
@@ -19,7 +22,8 @@ import androidx.navigation.ui.NavigationUI;
 public class MainActivity extends AppCompatActivity {
 
     private NavController controller;
-    private DrawerLayout mDrawerLayout;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,16 +34,21 @@ public class MainActivity extends AppCompatActivity {
         controller = host.getNavController();
 
         NavigationView navView = findViewById(R.id.nav_view);
-        NavigationUI.setupWithNavController(navView, controller);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        ActionBar actionbar = getSupportActionBar();
-        actionbar.setDisplayHomeAsUpEnabled(true);
-        actionbar.setHomeAsUpIndicator(R.drawable.about_black);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationUI.setupActionBarWithNavController(this, controller, drawerLayout);
 
-        mDrawerLayout = findViewById(R.id.drawer_layout);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(drawerToggle);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
     }
 
     @Override
@@ -51,12 +60,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
+        if (drawerToggle.onOptionsItemSelected(item))
+            return true;
         switch (id) {
             case R.id.about_item:
                 controller.navigate(R.id.aboutFragment);
                 return true;
             case android.R.id.home:
-                mDrawerLayout.openDrawer(GravityCompat.START);
+                drawerLayout.openDrawer(GravityCompat.START);
                 return true;
         }
         return super.onOptionsItemSelected(item);
