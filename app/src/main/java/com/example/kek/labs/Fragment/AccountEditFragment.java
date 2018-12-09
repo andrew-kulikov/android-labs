470b13891bd633cb6921a0761551bc4d2c5e0932
Andrew <andrew.1.kulikov@gmail.com>
2018-12-09 17:35:09 +0300

    Add logic to edit account fragment

--- app/src/main/java/com/example/kek/labs/Fragment/AccountEditFragment.java
+++ app/src/main/java/com/example/kek/labs/Fragment/AccountEditFragment.java
@@ -1,50 +1,117 @@
 package com.example.kek.labs.Fragment;
 
+import android.Manifest;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.example.kek.labs.R;
+import com.example.kek.labs.Util.ImageManager;
+import com.example.kek.labs.Util.PermissionManager;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.example.kek.labs.R;
-import com.example.kek.labs.Util.GlideApp;
+import static android.app.Activity.RESULT_OK;
 
 public class AccountEditFragment extends Fragment {
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_LOAD_IMAGE = 2;
+    private static final int REQUEST_WRITE = 3;
     private View editView;
-
+    private ImageView logo;
+    private PermissionManager permissionManager;
+    private ImageManager imageManager;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         editView = inflater.inflate(R.layout.account_edit_fragment, container, false);
 
-       ImageView logo = editView.findViewById(R.id.accountLogo);
+        logo = editView.findViewById(R.id.accountLogo);
 
-        GlideApp.with(getContext())
-                .load(getLogoDirectoryPath() + "/logo.jpg")
-                .diskCacheStrategy(DiskCacheStrategy.NONE)
-                .skipMemoryCache(true)
-                .error(R.drawable.about)
-                .into(logo);
+        logo = editView.findViewById(R.id.accountLogo);
+        editView.findViewById(R.id.save_info_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+        String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_PHONE_STATE};
+
+        permissionManager = new PermissionManager(this);
+        permissionManager.requestPermissions(permissions, REQUEST_WRITE);
+
+        imageManager = new ImageManager(this.getActivity());
+        imageManager.LoadImage(
+                logo,
+                imageManager.getLogoDirectoryPath() + "/logo.jpg",
+                R.drawable.about);
 
         return editView;
     }
 
-    private String getLogoDirectoryPath() {
-        return Environment.getExternalStorageDirectory()
-                + "/Android/data/"
-                + getActivity().getApplicationContext().getPackageName()
-                + "/Files";
-    }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            Intent intent = imageManager.getPickImageIntent(REQUEST_IMAGE_CAPTURE, REQUEST_IMAGE_CAPTURE);
+            startActivityForResult(intent, REQUEST_LOAD_IMAGE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_LOAD_IMAGE: {
+                if (resultCode != RESULT_OK)
+                    return;
+
+                Uri imageUri = data.getData();
+
+                if (imageUri != null) {
+                    logo.setImageURI(imageUri);
+
+                    Bitmap bmp = ((BitmapDrawable) logo.getDrawable()).getBitmap();
+
+                    imageManager.storeImage(bmp);
+                } else {
+                    Bitmap takenPhoto = (Bitmap) data.getExtras().get("data");
+
+                    logo.setImageBitmap(takenPhoto);
+                    imageManager.storeImage(takenPhoto);
+                }
+
+                break;
+            }
+            case REQUEST_IMAGE_CAPTURE: {
+                Bitmap takenPhoto = (Bitmap) data.getExtras().get("data");
+
+                logo.setImageBitmap(takenPhoto);
+                imageManager.storeImage(takenPhoto);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        permissionManager.requestPermissions(permissions, REQUEST_WRITE);
     }
 }
--- app/src/main/java/com/example/kek/labs/Fragment/AccountInfoFragment.java
+++ app/src/main/java/com/example/kek/labs/Fragment/AccountInfoFragment.java
@@ -1,12 +1,6 @@
 package com.example.kek.labs.Fragment;
 
-import android.Manifest;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,46 +8,38 @@ import android.widget.ImageView;
 
 import com.example.kek.labs.R;
 import com.example.kek.labs.Util.ImageManager;
-import com.example.kek.labs.Util.PermissionManager;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
-import static android.app.Activity.RESULT_OK;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 public class AccountInfoFragment extends Fragment {
-    private static final int REQUEST_IMAGE_CAPTURE = 1;
-    private static final int REQUEST_LOAD_IMAGE = 2;
-    private static final int REQUEST_WRITE = 3;
-    private View infoView;
-    private ImageView logo;
-    private PermissionManager permissionManager;
-    private ImageManager imageManager;
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        infoView = inflater.inflate(R.layout.account_info_fragment, container, false);
+    public View onCreateView(@NonNull final LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View infoView = inflater.inflate(R.layout.account_info_fragment, container, false);
 
-        logo = infoView.findViewById(R.id.accountLogo);
+        ImageView logo = infoView.findViewById(R.id.accountLogo);
+        ImageManager imageManager = new ImageManager(getActivity());
+
+        imageManager.LoadImage(logo,
+                imageManager.getLogoDirectoryPath() + "/logo.jpg",
+                R.drawable.about);
+
+        NavHostFragment host = (NavHostFragment) getActivity()
+                .getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        final NavController controller = host.getNavController();
+
         infoView.findViewById(R.id.selectLogoButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dispatchTakePictureIntent();
+                controller.navigate(R.id.accountEditFragment);
             }
         });
-
-        String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_PHONE_STATE};
-
-        permissionManager = new PermissionManager(this);
-        permissionManager.requestPermissions(permissions, REQUEST_WRITE);
-
-        imageManager = new ImageManager(this.getActivity());
-        imageManager.LoadImage(
-                logo,
-                imageManager.getLogoDirectoryPath() + "/logo.jpg",
-                R.drawable.about);
 
         return infoView;
     }
@@ -61,54 +47,5 @@ public class AccountInfoFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-    }
-
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
-            Intent intent = imageManager.getPickImageIntent(REQUEST_IMAGE_CAPTURE, REQUEST_IMAGE_CAPTURE);
-            startActivityForResult(intent, REQUEST_LOAD_IMAGE);
-        }
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            case REQUEST_LOAD_IMAGE: {
-                if (resultCode != RESULT_OK)
-                    return;
-
-                Uri imageUri = data.getData();
-
-                if (imageUri != null) {
-                    logo.setImageURI(imageUri);
-
-                    Bitmap bmp = ((BitmapDrawable) logo.getDrawable()).getBitmap();
-
-                    imageManager.storeImage(bmp);
-                } else {
-                    Bitmap takenPhoto = (Bitmap) data.getExtras().get("data");
-
-                    logo.setImageBitmap(takenPhoto);
-                    imageManager.storeImage(takenPhoto);
-                }
-
-                break;
-            }
-            case REQUEST_IMAGE_CAPTURE: {
-                Bitmap takenPhoto = (Bitmap) data.getExtras().get("data");
-
-                logo.setImageBitmap(takenPhoto);
-                imageManager.storeImage(takenPhoto);
-            }
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        permissionManager.requestPermissions(permissions, REQUEST_WRITE);
     }
 }
