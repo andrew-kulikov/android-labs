e9d6bb06eeef8fab05d244ce4b568f8cf1b0fbf3
Andrew <andrew.1.kulikov@gmail.com>
2018-12-09 17:13:19 +0300

    Move image logic to separate class

--- app/src/main/java/com/example/kek/labs/Fragment/AccountInfoFragment.java
+++ app/src/main/java/com/example/kek/labs/Fragment/AccountInfoFragment.java
@@ -1,35 +1,20 @@
 package com.example.kek.labs.Fragment;
 
 import android.Manifest;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.Parcelable;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.example.kek.labs.R;
-import com.example.kek.labs.Util.GlideApp;
+import com.example.kek.labs.Util.ImageManager;
 import com.example.kek.labs.Util.PermissionManager;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -44,40 +29,7 @@ public class AccountInfoFragment extends Fragment {
     private View infoView;
     private ImageView logo;
     private PermissionManager permissionManager;
-
-    private static Intent getPickImageIntent(Context context) {
-        Intent chooserIntent = null;
-
-        List<Intent> intentList = new ArrayList<>();
-
-        Intent pickIntent = new Intent(Intent.ACTION_PICK,
-                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        pickIntent.putExtra("requestCode", REQUEST_LOAD_IMAGE);
-        Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        takePhotoIntent.putExtra("return-data", true);
-        takePhotoIntent.putExtra("requestCode", REQUEST_IMAGE_CAPTURE);
-        intentList = addIntentsToList(context, intentList, pickIntent);
-        intentList = addIntentsToList(context, intentList, takePhotoIntent);
-
-        if (intentList.size() > 0) {
-            chooserIntent = Intent.createChooser(intentList.remove(intentList.size() - 1),
-                    context.getString(R.string.edit_account));
-            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentList.toArray(new Parcelable[]{}));
-        }
-
-        return chooserIntent;
-    }
-
-    private static List<Intent> addIntentsToList(Context context, List<Intent> list, Intent intent) {
-        List<ResolveInfo> resInfo = context.getPackageManager().queryIntentActivities(intent, 0);
-        for (ResolveInfo resolveInfo : resInfo) {
-            String packageName = resolveInfo.activityInfo.packageName;
-            Intent targetedIntent = new Intent(intent);
-            targetedIntent.setPackage(packageName);
-            list.add(targetedIntent);
-        }
-        return list;
-    }
+    private ImageManager imageManager;
 
     @Nullable
     @Override
@@ -97,12 +49,11 @@ public class AccountInfoFragment extends Fragment {
         permissionManager = new PermissionManager(this);
         permissionManager.requestPermissions(permissions, REQUEST_WRITE);
 
-        GlideApp.with(getContext())
-                .load(getLogoDirectoryPath() + "/logo.jpg")
-                .diskCacheStrategy(DiskCacheStrategy.NONE)
-                .skipMemoryCache(true)
-                .error(R.drawable.about)
-                .into(logo);
+        imageManager = new ImageManager(this.getActivity());
+        imageManager.LoadImage(
+                logo,
+                imageManager.getLogoDirectoryPath() + "/logo.jpg",
+                R.drawable.about);
 
         return infoView;
     }
@@ -115,7 +66,7 @@ public class AccountInfoFragment extends Fragment {
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
-            Intent intent = getPickImageIntent(getActivity());
+            Intent intent = imageManager.getPickImageIntent(REQUEST_IMAGE_CAPTURE, REQUEST_IMAGE_CAPTURE);
             startActivityForResult(intent, REQUEST_LOAD_IMAGE);
         }
     }
@@ -134,56 +85,24 @@ public class AccountInfoFragment extends Fragment {
 
                     Bitmap bmp = ((BitmapDrawable) logo.getDrawable()).getBitmap();
 
-                    storeImage(bmp);
+                    imageManager.storeImage(bmp);
                 } else {
                     Bitmap takenPhoto = (Bitmap) data.getExtras().get("data");
 
                     logo.setImageBitmap(takenPhoto);
-                    storeImage(takenPhoto);
+                    imageManager.storeImage(takenPhoto);
                 }
 
                 break;
             }
+            case REQUEST_IMAGE_CAPTURE: {
+                Bitmap takenPhoto = (Bitmap) data.getExtras().get("data");
+
+                logo.setImageBitmap(takenPhoto);
+                imageManager.storeImage(takenPhoto);
+            }
         }
     }
-
-    private String getLogoDirectoryPath() {
-        return Environment.getExternalStorageDirectory()
-                + "/Android/data/"
-                + getActivity().getApplicationContext().getPackageName()
-                + "/Files";
-    }
-
-    private File getOutputMediaFile() {
-        File mediaStorageDir = new File(getLogoDirectoryPath());
-
-        if (!mediaStorageDir.exists())
-            if (!mediaStorageDir.mkdirs())
-                return null;
-
-        File mediaFile;
-        String mImageName = "logo.jpg";
-        mediaFile = new File(mediaStorageDir.getPath() + File.separator + mImageName);
-        return mediaFile;
-    }
-
-    private void storeImage(Bitmap image) {
-        File pictureFile = getOutputMediaFile();
-        if (pictureFile == null) {
-            return;
-        }
-        try {
-            FileOutputStream fos = new FileOutputStream(pictureFile, false);
-            image.compress(Bitmap.CompressFormat.PNG, 90, fos);
-            fos.close();
-        } catch (FileNotFoundException e) {
-            Log.d("Sdf", "File not found: " + e.getMessage());
-        } catch (IOException e) {
-            Log.d("sdfsd", "Error accessing file: " + e.getMessage());
-        }
-    }
-
-
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
---
+++ app/src/main/java/com/example/kek/labs/Util/ImageManager.java
@@ -0,0 +1,109 @@
+package com.example.kek.labs.Util;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.os.Parcelable;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.ImageView;
+
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.example.kek.labs.R;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImageManager {
+    private Activity activity;
+
+    public ImageManager(Activity activity) {
+        this.activity = activity;
+    }
+
+    public Intent getPickImageIntent(int requestCameraCode, int requestLoadCode) {
+        Intent chooserIntent = null;
+
+        List<Intent> intentList = new ArrayList<>();
+
+        Intent pickIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        pickIntent.putExtra("requestCode", requestLoadCode);
+        Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        takePhotoIntent.putExtra("return-data", true);
+        takePhotoIntent.putExtra("requestCode", requestCameraCode);
+        intentList = addIntentsToList(intentList, pickIntent);
+        intentList = addIntentsToList(intentList, takePhotoIntent);
+
+        if (intentList.size() > 0) {
+            chooserIntent = Intent.createChooser(intentList.remove(intentList.size() - 1),
+                    activity.getString(R.string.edit_account));
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentList.toArray(new Parcelable[]{}));
+        }
+
+        return chooserIntent;
+    }
+
+    private List<Intent> addIntentsToList(List<Intent> list, Intent intent) {
+        List<ResolveInfo> resInfo = activity.getPackageManager().queryIntentActivities(intent, 0);
+        for (ResolveInfo resolveInfo : resInfo) {
+            String packageName = resolveInfo.activityInfo.packageName;
+            Intent targetedIntent = new Intent(intent);
+            targetedIntent.setPackage(packageName);
+            list.add(targetedIntent);
+        }
+        return list;
+    }
+
+    public String getLogoDirectoryPath() {
+        return Environment.getExternalStorageDirectory()
+                + "/Android/data/"
+                + activity.getApplicationContext().getPackageName()
+                + "/Files";
+    }
+
+    public void LoadImage(ImageView to, String path, int alternative) {
+        GlideApp.with(activity)
+                .load(path)
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                .skipMemoryCache(true)
+                .error(alternative)
+                .into(to);
+    }
+
+    private File getOutputMediaFile() {
+        File mediaStorageDir = new File(getLogoDirectoryPath());
+
+        if (!mediaStorageDir.exists())
+            if (!mediaStorageDir.mkdirs())
+                return null;
+
+        File mediaFile;
+        String mImageName = "logo.jpg";
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator + mImageName);
+        return mediaFile;
+    }
+
+    public void storeImage(Bitmap image) {
+        File pictureFile = getOutputMediaFile();
+        if (pictureFile == null) {
+            return;
+        }
+        try {
+            FileOutputStream fos = new FileOutputStream(pictureFile, false);
+            image.compress(Bitmap.CompressFormat.PNG, 90, fos);
+            fos.close();
+        } catch (FileNotFoundException e) {
+            Log.d("Sdf", "File not found: " + e.getMessage());
+        } catch (IOException e) {
+            Log.d("sdfsd", "Error accessing file: " + e.getMessage());
+        }
+    }
+}
