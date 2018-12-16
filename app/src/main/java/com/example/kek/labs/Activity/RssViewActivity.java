e2c9c441cf05ca8a4cc1faee1e5e891709b9df43
Andrew <andrew.1.kulikov@gmail.com>
2018-12-17 01:59:47 +0300

    Add link opening on news click

--- app/src/main/java/com/example/kek/labs/Activity/RssViewActivity.java
+++ app/src/main/java/com/example/kek/labs/Activity/RssViewActivity.java
@@ -2,7 +2,9 @@ package com.example.kek.labs.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 import com.example.kek.labs.R;
 
@@ -12,5 +14,11 @@ public class RssViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rss_view);
+
+        Intent intent = getIntent();
+        String link = intent.getStringExtra("link");
+        WebView webView = findViewById(R.id.rss_web_view);
+        //webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(link);
     }
 }
--- app/src/main/java/com/example/kek/labs/Adapters/RssNewsAdapter.java
+++ app/src/main/java/com/example/kek/labs/Adapters/RssNewsAdapter.java
@@ -1,12 +1,14 @@
 package com.example.kek.labs.Adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.kek.labs.Activity.RssViewActivity;
 import com.example.kek.labs.Models.FeedItem;
 import com.example.kek.labs.R;
 import com.example.kek.labs.Util.GlideApp;
@@ -17,6 +19,7 @@ import java.util.List;
 import java.util.Locale;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class RssNewsAdapter extends RecyclerView.Adapter<RssNewsAdapter.ViewHolder> {
@@ -39,7 +42,7 @@ public class RssNewsAdapter extends RecyclerView.Adapter<RssNewsAdapter.ViewHold
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
         SimpleDateFormat baseFormat = new SimpleDateFormat("E, d MMM yyyy HH:mm:ss Z", Locale.US);
         SimpleDateFormat endFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.US);
         String date = rssRecords.get(position).getPubDate();
@@ -54,6 +57,15 @@ public class RssNewsAdapter extends RecyclerView.Adapter<RssNewsAdapter.ViewHold
         GlideApp.with(context)
                 .load(rssRecords.get(position).getThumbnailUrl())
                 .into(holder.imageView);
+
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, RssViewActivity.class);
+                intent.putExtra("link", rssRecords.get(position).getLink());
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -69,12 +81,14 @@ public class RssNewsAdapter extends RecyclerView.Adapter<RssNewsAdapter.ViewHold
         TextView titleTextView;
         TextView dateTextView;
         ImageView imageView;
+        CardView cardView;
 
         ViewHolder(View v) {
             super(v);
             titleTextView = v.findViewById(R.id.card_title_text_view);
             dateTextView = v.findViewById(R.id.card_date_text_view);
             imageView = v.findViewById(R.id.card_image);
+            cardView = v.findViewById(R.id.rss_card);
         }
     }
 }
